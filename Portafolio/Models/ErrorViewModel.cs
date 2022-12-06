8153b988fce2b262b20e5dd5bd5e44f26e0b9edc
jacunar <josueally@gmail.com>
2022-12-05 18:33:05 -0600

    Creando proyecto portafolio

---
+++ Portafolio/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Portafolio.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
